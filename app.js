dfdfc751ff39e0ca2b5a65c5cef6e777240cfb56
Naved khan <nomanali50997@gmail.com>
2024-01-07 11:59:41 +0530

    making create add, edit ,show and delete route

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing  = require("./models/listing.js")
 const path = require("path");
-
+const methodOverride = require("method-override");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/airbnb";
 
@@ -20,6 +20,7 @@ async function main(){
 app.set("view engine", "ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}))
+app.use(methodOverride("_method"));
 
 app.get("/", (req,res)=>{
     res.send("you are on the root node");
@@ -31,11 +32,47 @@ app.get("/listings",async (req,res)=>{
     res.render("listings/index.ejs",{allListings});
 });
 
+
+// NEW ROUTE
+app.get("/listing/new",(req,res)=>{
+    res.render("listings/new.ejs")
+})
+
 // SHOW ROUTE
 app.get("/listings/:id",async (req,res)=>{
     let { id } = req.params;
     const listing= await Listing.findById(id);
     res.render("listings/show.ejs", {listing})
+})
+
+// CREATE ROUTE
+app.post("/listings" ,async (req,res)=>{
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings")
+})
+
+// EDIT ROUTE
+app.get("/listings/:id/edit" , async(req,res)=>{
+    let { id } = req.params;
+    const listing= await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+});
+
+
+// UPDATE ROUTE
+app.put("/listings/:id", async(req,res)=>{
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id ,{...req.body.listing});
+    res.redirect(`/listings/${id}`);
+})
+
+//DELETE ROUTE
+app.delete("/listings/:id",async(req,res)=>{
+    let { id } = req.params;
+    let deletedListing= await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
 })
 
 // app.get("/testListing", async (req,res)=>{
