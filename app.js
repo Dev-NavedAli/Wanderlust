b613a0efcefe4a112fe196a3e7dc02272fdf67c0
Naved khan <nomanali50997@gmail.com>
2024-01-08 12:23:37 +0530

    designing navbar and footer and template it on all pages using ejsmate

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const Listing  = require("./models/listing.js")
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/airbnb";
 
@@ -21,6 +22,8 @@ app.set("view engine", "ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride("_method"));
+app.engine('ejs',ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
 
 app.get("/", (req,res)=>{
     res.send("you are on the root node");
@@ -42,7 +45,7 @@ app.get("/listing/new",(req,res)=>{
 app.get("/listings/:id",async (req,res)=>{
     let { id } = req.params;
     const listing= await Listing.findById(id);
-    res.render("listings/show.ejs", {listing})
+    res.render("listings/show.ejs", { listing })
 })
 
 // CREATE ROUTE
@@ -65,7 +68,7 @@ app.put("/listings/:id", async(req,res)=>{
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id ,{...req.body.listing});
     res.redirect(`/listings/${id}`);
-})
+});
 
 //DELETE ROUTE
 app.delete("/listings/:id",async(req,res)=>{
@@ -73,7 +76,7 @@ app.delete("/listings/:id",async(req,res)=>{
     let deletedListing= await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
     res.redirect("/listings");
-})
+});
 
 // app.get("/testListing", async (req,res)=>{
 //     let sampleListing = new Listing({
