85d553bdcabf95a39948d1bb6683e70c2fc52eaa
Naved khan <nomanali50997@gmail.com>
2024-01-19 12:34:09 +0530

    Solve the error in Validation Schema and Using error handling

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const { listingSchema} = require("./schema.js");
+const { listingSchema , reviewSchema} = require("./schema.js");
 const Review = require("./models/review.js")
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/airbnb";
@@ -35,17 +35,31 @@ app.get("/", (req, res) => {
 
 const validateListing = (req,res,next) =>{
     let {error}= listingSchema.validate(req.body);
-    let errMsg = error.details.map((el) => el.message).join(",");
     if (error){
+    let errMsg = error.details.map((el) => el.message).join(",");
         throw new ExpressError(400,errMsg);
     }else{
         next();
     }
-}
+};
+
+
+
+const validateReview = (req,res,next) =>{
+    let { error } = reviewSchema.validate(req.body);
+    if(error){
+        let errMsg = error.details.map((el)=> el.message).join(",");
+        throw new ExpressError(400,errMsg);
+    } else{
+        next();
+    }
+};
 
 
 //INDEX ROUTE
-app.get("/listings", wrapAsync(async (req, res) => {
+app.get(
+    "/listings",
+    wrapAsync(async (req, res) => {
     const allListings = await Listing.find({});
     res.render("listings/index.ejs", { allListings });
 })
@@ -77,7 +91,9 @@ app.post(
 );
 
 // EDIT ROUTE
-app.get("/listings/:id/edit",wrapAsync (async(req, res) => {
+app.get(
+    "/listings/:id/edit",
+    wrapAsync (async(req, res) => {
         let { id } = req.params;
         const listing = await Listing.findById(id);
         res.render("listings/edit.ejs", { listing });
@@ -95,9 +111,11 @@ app.put("/listings/:id",
 })
 );
 
+
 //DELETE ROUTE
 app.delete(
-    "/listings/:id", wrapAsync(async (req, res) => {
+    "/listings/:id",
+    wrapAsync(async (req, res) => {
     let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
@@ -105,14 +123,13 @@ app.delete(
 })
 );
 
+
 // Reviews
 // Post route 
-
-app.post(
-    "/listings/:id/reviews",
-    async(req,res)=>{
+app.post("/listings/:id/reviews",validateReview,wrapAsync(async(req,res)=>{
     let listing = await Listing.findById(req.params.id);  //hum us listing ko acces krenge jiski id yha hmne di hai
     let newReview = new Review(req.body.review);
+
     listing.reviews.push(newReview);
 
     await newReview.save();
@@ -120,7 +137,7 @@ app.post(
 
     console.log("new review saved ");
     res.redirect(`/listings`); //not match as  mentors projects (`/listings/${listing._id}`);
-});
+}));
 
 // app.get("/testListing", async (req,res)=>{
 //     let sampleListing = new Listing({
