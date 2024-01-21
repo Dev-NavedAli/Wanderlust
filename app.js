c8c94202c433b7cae3cfd26afc57d6e278e195a7
Naved khan <nomanali50997@gmail.com>
2024-01-21 11:48:20 +0530

    Rendering reviews

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ app.get("/listing/new", (req, res) => {
 // SHOW ROUTE
 app.get("/listings/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
-    const listing = await Listing.findById(id);
+    const listing = await Listing.findById(id).populate("reviews");
     res.render("listings/show.ejs", { listing })
 })
 );
