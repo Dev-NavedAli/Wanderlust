910ea450fc4e34fcfb5ace7f6f424abeddc172e7
Naved khan <nomanali50997@gmail.com>
2024-01-30 12:04:37 +0530

    making the userSchema for authentication by passport

---
+++ models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const userSchema = new Schema({
+    email:{
+        type : String,
+        required : true
+    },
+});
+// You're free to define your User how you like. Passport-Local Mongoose by default add a username, hash and salt field to store the username, the hashed password and the salt value.
+User.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', userSchema);
