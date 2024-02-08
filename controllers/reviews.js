b24eabc23e6a2f12960828b652a30e4675f927c2
Naved khan <nomanali50997@gmail.com>
2024-02-08 15:47:31 +0530

    applying MVC format to reviews

---
+++ controllers/reviews.js
@@ -0,0 +1,25 @@
+const Listing = require("../models/listing.js");
+const Review = require("../models/review.js");
+
+module.exports.createReview = async(req,res)=>{
+    let listing = await Listing.findById(req.params.id);  //hum us listing ko acces krenge jiski id yha hmne di hai
+    let newReview = new Review(req.body.review);
+    newReview.author = req.user._id;
+    listing.reviews.push(newReview);
+    
+    await newReview.save();
+    await listing.save();
+    req.flash("success", "New Review Created!")
+
+    res.redirect(`/listings/${listing._id}`); //not match as  mentors projects (`/listings/${listing._id}`);
+}
+
+module.exports.destroyReview = async(req,res)=>{
+    let {id,reviewId} = req.params;
+
+    await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}}) //reviews array me jis bhi review se  reviewid match kar jaaye hum us id pull krna chchte hai 
+    await Review.findById(reviewId);
+    req.flash("success", "Review Deleted")
+
+    res.redirect(`/listings/${id}`);
+};
--- routes/review.js
+++ routes/review.js
@@ -4,27 +4,17 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
-const {validateReview, isLoggedIn, isReviewAuthor} = require("../middleware.js")
+const {validateReview, isLoggedIn, isReviewAuthor} = require("../middleware.js");
+const { createReview } = require("../controllers/reviews.js");
 
-
+const reviewController = require("../controllers/reviews.js")
 
 //  Post  Review route   ......chlid route
 router.post(
     "/",
     isLoggedIn,
     validateReview,
-    wrapAsync(async(req,res)=>{
-    let listing = await Listing.findById(req.params.id);  //hum us listing ko acces krenge jiski id yha hmne di hai
-    let newReview = new Review(req.body.review);
-    newReview.author = req.user._id;
-    listing.reviews.push(newReview);
-    
-    await newReview.save();
-    await listing.save();
-    req.flash("success", "New Review Created!")
-
-    res.redirect(`/listings/${listing._id}`); //not match as  mentors projects (`/listings/${listing._id}`);
-})
+    wrapAsync(reviewController.createReview)
 );
 
 //DELETE REVIEW ROUTE    .....child route
@@ -32,14 +22,6 @@ router.delete(
     "/:reviewId",
     isLoggedIn,
     isReviewAuthor,
-    wrapAsync(async(req,res)=>{
-    let {id,reviewId} = req.params;
-
-    await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}}) //reviews array me jis bhi review se  reviewid match kar jaaye hum us id pull krna chchte hai 
-    await Review.findById(reviewId);
-    req.flash("success", "Review Deleted")
-
-    res.redirect(`/listings/${id}`);
-}))
-
+    wrapAsync(reviewController.destroyReview)
+);
 module.exports = router;
