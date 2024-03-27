2e04843ee55d23e5d23817d8a731f400f376b3c4
Naved khan <nomanali50997@gmail.com>
2024-03-27 11:43:13 +0530

    addition in app.js

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ async function main() {
     await mongoose.connect(dbUrl);
 }
 
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
